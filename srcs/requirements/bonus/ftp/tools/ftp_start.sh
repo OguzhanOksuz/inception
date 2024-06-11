78173e0b1fdd4749bb3945820dd1e5bcf526f243
OguzhanOksuz <oguzhanoksuz0@gmail.com>
2024-06-12 02:00:49 +0300

    sleep

---
+++ srcs/requirements/bonus/ftp/tools/ftp_start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]
+then
+
+    mkdir -p /var/www/html
+
+    cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
+    mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
+
+    adduser $FTP_USER --disabled-password
+    chown -R $FTP_USER:$FTP_USER /var/www/html
+
+
+fi
+
