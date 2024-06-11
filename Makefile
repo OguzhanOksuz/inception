47f04d3c57f3cec257b0af4efdc7b59aa336bec8
OguzhanOksuz <oguzhanoksuz0@gmail.com>
2024-06-11 23:02:12 +0300

    .env to maraidb

--- Makefile
+++ Makefile
@@ -3,21 +3,23 @@ all: up
 
 up:
 	mkdir -p /home/data/wordpress
-	mkdir -p /home/data/mysql
-	docker-compose -f ./srcs/docker-compose.yml up -d --build
+	mkdir -p /home/data/mariadb
+	docker-compose -f ./srcs/docker-compose.yml up
 
 down:
 	docker-compose -f ./srcs/docker-compose.yml down
 
-clean: 
-	docker-compose -f ./srcs/docker-compose.yml down -v --remove-orphans
-	docker stop $$(docker ps -q)
-	docker rm $$(docker ps -a -q)
-	docker rmi -f $$(docker images -q)
-	rm -rf /home/data/wordpress
-	rm -rf /home/data/mysql
+clean:
+	docker stop $$(docker ps -qa)			|| true
+	docker rm $$(docker ps -qa)			|| true
+	docker rmi -f $$(docker images -q)		|| true
+	docker volume rm $$(docker volume ls -q)	|| true
+	docker network rm $$(docker network ls -q)	|| true
+	rm -rf /home/data/wordpress			|| true
+	rm -rf /home/data/mysql				|| true
 	
-re: clean all
+re:
+	docker-compose -f ./srcs/docker-compose.yml up --build
 
 .PHONY: all up down clean re
 
--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -14,14 +14,11 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	cd /var/www/html;
 	wp core download --allow-root;
 	mv /var/www/wp-config.php /var/www/html/
-
-	wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_HOST} --admin_user=${WP_USERNAME} --admin_password=${WP_PASSWORD} --admin_email=${WP_MAIL}
-	wp user create --allow-root ${WP_USERNAME} ${WP_MAIL} --user_pass=${WP_PASSWORD};
-	wp user create --allow-root ${WP_GUESTUSER} ${WP_GUESTMAIL} --role=subscriber --user_pass=${WP_GUESTPASS}
+	
+	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL};
+	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD}
 	wp plugin install redis-cache --activate --allow-root
-  	wp plugin update --all --allow-root
- 	wp plugin activate redis-cache --allow-root
-  	wp redis enable --force --allow-root
+ 	wp plugin update --all --allow-root
 fi
 
 exec "$@"
